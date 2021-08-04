c67de27a9dd1d35253b995d6d2f9b491815a0a98
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-08-04 14:17:07 -0500

    add export

---
+++ studio/Astronaut.ts
@@ -0,0 +1,10 @@
+export class Astronaut {}
+
+class Astronaut implements Payload {
+    name:string;
+    massKg: number;
+    constructor(name:string, masskg:number){
+        this.name = name;
+        this.masskg = maskg;       
+   
+}
